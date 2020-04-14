e6cf8fa5d2a451118a73ef8e11e3bbdc9d8160ca
boseji <boseji@users.noreply.github.com>
2020-04-14 23:43:15 +0530

    Adding nano config + correction for rsync - Great !:star:

---
+++ nanorc.sh
@@ -0,0 +1,41 @@
+include "/usr/share/nano-syntax-highlighting/*.nanorc"
+# Eable Line Numbers
+set linenumbers
+# Enable Case sensitive Searching by default
+set casesensitive
+# Show the cursor location in status
+set constantshow
+# Remember the edit location for last 200 files
+set positionlog
+# Softwrapping lines
+set softwrap
+set atblanks
+# Smarly Home for code editing
+set smarthome
+# Verticial Line at 80
+set guidestripe 80
+# Allow to Suspend
+set suspendable
+# Remove Blank spaces at end of lines
+#set trimblanks
+# Convert file to linux line ending
+#set unix
+
+# Theme
+# set element fgcolor,bgcolor
+
+set titlecolor brightwhite,blue
+
+set statuscolor brightwhite,green
+
+set errorcolor brightwhite,red
+
+set selectedcolor brightwhite,magenta
+
+set stripecolor ,yellow
+
+set numbercolor cyan
+
+set keycolor cyan
+
+set functioncolor green
